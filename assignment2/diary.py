fcbe6a9226b2c7fd5197fc043787320fc0203b79
kmi8a <168780154+kmi8a@users.noreply.github.com>
2026-04-09 10:51:56 -0400

    completed task 1 - Diary

---
+++ assignment2/diary.py
@@ -0,0 +1,20 @@
+import traceback
+
+diary_input = ''
+special_line = 'done for now'
+
+with open('diary.txt', 'a')as file:
+    try:
+        while diary_input != special_line:
+            diary_input = input('What happened today? ')
+            file.write(f'{diary_input}\n')
+    except Exception as e:
+        trace_back = traceback.extract_tb(e.__traceback__)
+        stack_trace = list()
+        for trace in trace_back:
+            stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+        print(f"Exception type: {type(e).__name__}")
+        message = str(e)
+        if message:
+            print(f"Exception message: {message}")
+        print(f"Stack trace: {stack_trace}")
